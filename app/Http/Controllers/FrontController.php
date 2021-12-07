c19fbe4aa6593b49ffccab043e68ec4287f84b4c
jang700 <jangwenjen@gmail.com>
2021-12-07 17:21:39 +0800

    controllers

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    //
+    public function index() 
+    {
+        return view('index');
+
+    }
+    public function hi()
+    {
+        $name = 'jang';
+        $age = 18;
+        $gender = 'female';
+    
+        // dd(compact('name','age','gender'));
+        return view('hi',compact('name','age','gender'));
+    }
+
+}
